acbc313e661dbdbc716c5b0c897c88eb72023ac5
Evan <evan.lajusticia@gmail.com>
2023-09-18 22:34:31 +0200

    added logic for "save team" option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CharactersService } from './services/characters.service';
+import { DEFAULT_CHARACTER_LIST_SIZE } from 'src/models/constants';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,24 @@ import { CharactersService } from './services/characters.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   constructor(public characters: CharactersService) {
-    for(let i = 0; i < 4; i++)
+    const characterList = JSON.parse(localStorage.getItem('characters'));
+
+    if (characterList && characterList.length > 0)
+      this.characters.list = characterList;
+
+    while(this.characters.list.length < DEFAULT_CHARACTER_LIST_SIZE)
       this.characters.addCharacter();
+  }
+
+  @HostListener('window:unload', ['$event'])
+  unloadHandler(event: Event) {
+    const shouldSaveCharacters = localStorage.getItem('options').includes('saveTeam');
+    
+    if(shouldSaveCharacters) {
+      const savedCharacters = this.characters.list.filter(character => character.ally);
+      localStorage.setItem('characters', JSON.stringify(savedCharacters));
+    }
   }
 }
