40f50f6c5378fa842f2693f2e312236e50683c93
Evan <evan.lajusticia@gmail.com>
2023-09-18 19:01:17 +0200

    added favicon and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { CharactersService } from './services/characters.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'roll-initiative';
-
   constructor(public characters: CharactersService) {
     for(let i = 0; i < 4; i++)
       this.characters.addCharacter();
