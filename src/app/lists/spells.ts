854b7ad17e810998f97b0654295b4aa5ab9d8637
Evan <evan.lajusticia@gmail.com>
2023-10-29 19:59:25 +0100

    removed character sheet stuff for first release of spell index

--- src/app/lists/spells.ts
+++ src/app/lists/spells.ts
@@ -5108,8 +5108,6 @@ export const ZONE_DE_VERITE: Spell = {
     "<p>Vous créez une zone magique, capable de protéger contre la duplicité, sous forme d'une sphère de 4,50 mètres de rayon centrée sur un point de votre choix situé à portée. Jusqu'à la fin du sort, une créature qui pénètre dans la sphère pour la première fois de son tour ou y commence son tour doit réussir un jet de sauvegarde de Charisme. Si elle échoue, elle ne peut pas mentir délibérément tant qu'elle reste dans la zone du sort. Vous savez si chaque créature présente a réussi ou raté son jet de sauvegarde.</p><br/><p>Une créature affectée est consciente du sort qui la limite et peut donc soigneusement éviter de répondre aux questions qui susciteraient normalement un mensonge de sa part. Une telle créature peut rester évasive dans ses réponses, tant qu'elle reste dans les limites de la vérité.</p><br/>",
 };
 
-
-
 /* ================================================= */
 /*                  LISTE DE SORTS                   */
 /* ================================================= */
