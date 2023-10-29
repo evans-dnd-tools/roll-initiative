991bcb83d05ed3c375910b215c83eaac7b5a2e0f
Evan <evan.lajusticia@gmail.com>
2023-10-29 20:23:54 +0100

    it works but now i need to know if `useHash` is mandatory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes)
   ],
   declarations: [],
   exports: [ RouterModule ]
